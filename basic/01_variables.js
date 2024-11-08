039ee25504a6438cadc4e2c63d10a6c19f2ab44e
Mohit Rawat <95868156+mohitrawt@users.noreply.github.com>
2024-11-08 14:54:20 +0000

    type of data in js : data_type

---
+++ basic/01_variables.js
@@ -0,0 +1,17 @@
+const pi = 1212;
+let lang = "java_script";
+var type = "web lang";
+name = "Mohit"; // var declaration : not a good practise
+
+// console.log(pi);
+// console.log(lang);
+// console.log(type);
+
+console.table([pi,"lang","type"]);
+/*
+NOTE:
+use let instead of var :-
+bcz of let have block level scope and var have functional scope : above the block level access it 
+
+*/
+console.log(name);
---
+++ basic/02_datatypes.js
@@ -0,0 +1,22 @@
+/* Data type : primitive
+Number 2 to the power of 53 
+BigInt
+String
+Boolean
+null - stand alone value
+undefined -
+symbol - unique
+
+data type : non primitive
+objects
+
+*/
+let name ="JS";
+let isLoged ="false";
+let age = null;
+let age1 ; // undefined
+console.log(typeof(name));
+console.log(typeof(null)); //null - object
+console.log(typeof(undefined));
+
+
