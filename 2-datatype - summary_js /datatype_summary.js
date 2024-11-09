ead10d21d01f7dc49eb5b3e1d0b3bde41b37aacb
Mohit Rawat <95868156+mohitrawt@users.noreply.github.com>
2024-11-09 02:57:24 +0000

    summary datatype

---
+++ 2-datatype - summary_js /datatype_summary.js
@@ -0,0 +1,41 @@
+// primitive 
+
+// 7 
+// number : BigInt : String :Boolean: null : undefined : symbol 
+
+const score = 200;
+const score2 = 300;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const id2 = Symbol("123");
+
+// console.log(id == id2);
+
+const Big_No = 9037822791790107910n;
+// console.log(Big_No);
+
+// reference (Non primitive)
+
+// Array , objects , function
+
+const heroes = ["spiderman","flash","Ironman"];
+console.log(heroes);
+
+// object
+ let obj ={
+    name:"mohit",
+    age:22,
+    course:"MCA",
+}
+//function 
+const myfun = function(){
+    console.log("function store in var");
+    
+}
+
+// NOTE : 
+// null - type  - object 
