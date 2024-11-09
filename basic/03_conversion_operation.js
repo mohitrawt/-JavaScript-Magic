22848810fe291aa4c5962c6566bac94323907da5
Mohit Rawat <95868156+mohitrawt@users.noreply.github.com>
2024-11-09 02:27:28 +0000

    javascript basic operation

---
+++ basic/03_conversion_operation.js
@@ -0,0 +1,63 @@
+let score = "33abc" //string 
+
+// console.log(typeof score);
+
+let numbr = Number(score);
+
+// console.log(typeof numbr);
+// console.log(numbr);
+  
+// "33" - 33
+// "33abc" - NAN 
+// true - 1 , false - 0
+
+// let isloggedIn =  0;  false
+let isloggedIn =  1;    // true
+let booleanIsloggedIN = Boolean(isloggedIn);
+
+// console.log(booleanIsloggedIN);
+
+// "" => false
+// "mohit"- true
+
+let sum_No = 33;
+// console.log(typeof sum_No);
+
+let StringNo = String(sum_No);
+
+// console.log( StringNo);
+
+
+// ****************OPERATION********************
+
+let val = 5
+let negVal = -val
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**3);
+
+let str = "helllo"
+let str1 = "Mohit"
+
+let str2 = str+ " "+ str1;
+// console.log(str2);
+
+// console.log(" 1 "+ 2);
+// console.log("1"+1+"1");
+// console.log(1+1+"1");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let n1 ,n2 , n3;
+n1 = n2 = n3 = 5
+
+console.log(n2);
+
+let game_counter = 100;
+game_counter++;
+console.log(game_counter++);
+console.log(game_counter);
+
