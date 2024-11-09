d8dcd349668ababdc3023d006d1cfb2551fde14b
Mohit Rawat <95868156+mohitrawt@users.noreply.github.com>
2024-11-09 02:09:22 +0000

    data type conversion

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # -JavaScript-Magic
 Description: A collection of JavaScript code examples, tutorials, and hands-on projects that turn you into a JavaScript wizard! Includes DOM manipulation, AJAX, and modern ES6+ features.
+
+Variables declaration learnig : let , var , const
---
+++ basic/03_conversion.js
@@ -0,0 +1,28 @@
+let score = "33abc" //string 
+
+console.log(typeof score);
+
+let numbr = Number(score);
+
+console.log(typeof numbr);
+console.log(numbr);
+  
+// "33" - 33
+// "33abc" - NAN 
+// true - 1 , false - 0
+
+// let isloggedIn =  0;  false
+let isloggedIn =  1;    // true
+let booleanIsloggedIN = Boolean(isloggedIn);
+
+console.log(booleanIsloggedIN);
+
+// "" => false
+// "mohit"- true
+
+let sum_No = 33;
+console.log(typeof sum_No);
+
+let StringNo = String(sum_No);
+
+console.log( StringNo);
